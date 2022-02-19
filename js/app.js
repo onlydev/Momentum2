ca730301b3363dcf6925ede2a353dba8b9dc93f3
onlydev <izen76@gmail.com>
2022-02-19 10:49:23 +0900

    오늘 날짜 및 시간
    시간에 따른 인사말 추가
    인사말, 명언, 요일을 함수로 분리

--- js/app.js
+++ js/app.js
@@ -1,77 +1,19 @@
-const images = [
-	"01.jpg",
-	"02.jpg",
-	"03.jpg",
-	"04.jpg",
-	"05.jpg",
-	"06.jpg",
-	"07.jpg",
-	"08.jpg",
-	"09.jpg",
-	"10.jpg",
-	"11.jpg",
-	"12.jpg",
-	"13.jpg",
-];
-
-const quotes = [
-	{
-		quote: "세상에 공짜는 없다.",
-		author: "미상",
-	},
-	{
-		quote: "최소한 다른 사람에게 피해를 주는 사람은 되지 말라.",
-		author: "미상",
-	},
-	{
-		quote: "박수칠 때 떠나라.",
-		author: "미상",
-	},
-	{
-		quote: "이것 또한 지나가리라.",
-		author: "유대 경전 - 미드라시",
-	},
-	{
-		quote: "실천하라.",
-		author: "나",
-	},
-	{
-		quote: "소년이여, 야망을 가져라.",
-		author: "윌리엄 스미스 클라크",
-	},
-	{
-		quote:
-			"일단 유명해져라. 그러면 당신이 똥을 싸도 사람들은 박수를 칠 것이다.",
-		author: "앤디 워홀",
-	},
-	{
-		quote: "승리해도 병신, 패배해도 병신이라면 승리한 병신이 되어라.",
-		author: "앨빈 토플러",
-	},
-	{
-		quote: "강한 자가 이기는 것이 아니라, 이긴 자가 강한 것이다.",
-		author: "프란츠 베켄바우어",
-	},
-	{
-		quote: "사느냐, 죽느냐, 그것이 문제로다.",
-		author: "햄릿 中",
-	},
-];
-
 window.addEventListener("load", function (event) {
 	// 랜덤 배경 가져오기
-	const image = images[getRandomArrayKey(images)];
+	const image = getImage();
 	document.body.style.backgroundImage = `url('./img/${image}')`;
 	document.body.style.backgroundRepeat = "no-repeat";
 	document.body.style.backgroundSize = "cover";
 
 	// 랜덤 명언 가져오기
-	const quoteItem = quotes[getRandomArrayKey(quotes)];
+	const quoteItem = getQuote();
 	const wiseSayingQuote = document.querySelector(".wise-saying .quote");
 	const wiseSayingAuthor = document.querySelector(".wise-saying .author");
 
-	wiseSayingQuote.innerText = quoteItem.quote;
+	wiseSayingQuote.innerText = `"${quoteItem.quote}"`;
 	wiseSayingAuthor.innerText = `- ${quoteItem.author}`;
+
+	setInterval(getRealTime, 1000);
 });
 
 /**
@@ -79,11 +21,41 @@ window.addEventListener("load", function (event) {
  *
  * Todo 새 창으로 열고 있지만 기본 창에서 움직여도 괜찮을지 생각해 보기
  */
- const googleSearch = document.querySelector(".js-search");
- googleSearch.addEventListener("keydown", function (e) {
-	 if (e.key === "Enter") {
-		 const keyword = googleSearch.value;
- 
-		 window.open(`https://www.google.com/search?q=${keyword}`, "searchTab");
-	 }
- });
+const googleSearch = document.querySelector(".js-search");
+googleSearch.addEventListener("keydown", function (e) {
+	if (e.key === "Enter") {
+		const keyword = googleSearch.value;
+
+		window.open(`https://www.google.com/search?q=${keyword}`, "searchTab");
+	}
+});
+
+// 실시간 시계
+function getRealTime() {
+	const today = new Date();
+
+	// 오늘 날짜 가져오기
+	const [year, month, day, yoil] = [
+		today.getFullYear(),
+		getTwoCharacterZeroPadded(today.getMonth() + 1),
+		getTwoCharacterZeroPadded(today.getDate()),
+		getYoil("en", today.getDay()),
+	];
+
+	const todayDate = document.querySelector(".date");
+	todayDate.innerText = `${year}.${month}.${day} (${yoil})`;
+
+	// 현재 시간 가져오기
+	const hour = getTwoCharacterZeroPadded(today.getHours());
+	const minute = getTwoCharacterZeroPadded(today.getMinutes());
+	const second = getTwoCharacterZeroPadded(today.getSeconds());
+
+	const realTime = document.querySelector(".clock");
+	realTime.innerText = `${hour}:${minute}:${second}`;
+
+	// 인사하기
+	const greeting = getGreeting(hour);
+
+	const viewGreeting = document.querySelector(".greeting");
+	viewGreeting.innerText = greeting;
+}
--- js/helper.js
+++ js/helper.js
@@ -7,3 +7,132 @@
 function getRandomArrayKey(arr) {
 	return Math.floor(Math.random() * arr.length);
 }
+
+/**
+ * * 넘어온 값을 0으로 채운 2자리의 문자열로 리턴
+ *
+ * @param {*} item
+ * @param {*} maxLength
+ * @param {*} fillString
+ * @returns
+ */
+function getTwoCharacterZeroPadded(item, maxLength = 2, fillString = "0") {
+	return String(item).padStart(maxLength, fillString);
+}
+
+/**
+ * * 시간에 따른 인삿말
+ *
+ * @param {*} hour
+ * @returns
+ */
+function getGreeting(hour) {
+	if (hour >= 6 && hour < 12) {
+		return "Good morning! ";
+	} else if (hour >= 12 && hour < 17) {
+		return "Good afternoon! ";
+	} else if (hour >= 17 && hour < 21) {
+		return "Good evening! ";
+	} else {
+		return "Good night! ";
+	}
+}
+
+/**
+ * * 명언 가져오기
+ *
+ * @returns
+ */
+function getQuote() {
+	const quotes = [
+		{
+			quote: "세상에 공짜는 없다.",
+			author: "미상",
+		},
+		{
+			quote: "최소한 다른 사람에게 피해를 주는 사람은 되지 말라.",
+			author: "미상",
+		},
+		{
+			quote: "박수칠 때 떠나라.",
+			author: "미상",
+		},
+		{
+			quote: "이것 또한 지나가리라.",
+			author: "유대 경전 - 미드라시",
+		},
+		{
+			quote: "생각은 깊게, 행동은 빠르게! (실천하라)",
+			author: "나",
+		},
+		{
+			quote: "소년이여, 야망을 가져라.",
+			author: "윌리엄 스미스 클라크",
+		},
+		{
+			quote:
+				"일단 유명해져라. 그러면 당신이 똥을 싸도 사람들은 박수를 칠 것이다.",
+			author: "앤디 워홀",
+		},
+		{
+			quote: "승리해도 병신, 패배해도 병신이라면 승리한 병신이 되어라.",
+			author: "앨빈 토플러",
+		},
+		{
+			quote: "강한 자가 이기는 것이 아니라, 이긴 자가 강한 것이다.",
+			author: "프란츠 베켄바우어",
+		},
+		{
+			quote: "사느냐, 죽느냐, 그것이 문제로다.",
+			author: "햄릿 中",
+		},
+	];
+
+	return quotes[getRandomArrayKey(quotes)];
+}
+
+/**
+ * * 배경 이미지 가져오기
+ *
+ * @returns
+ */
+function getImage() {
+	const images = [
+		"01.jpg",
+		"02.jpg",
+		"03.jpg",
+		"04.jpg",
+		"05.jpg",
+		"06.jpg",
+		"07.jpg",
+		"08.jpg",
+		"09.jpg",
+		"10.jpg",
+		"11.jpg",
+		"12.jpg",
+		"13.jpg",
+	];
+
+	return images[getRandomArrayKey(images)];
+}
+
+/**
+ * * 현재 시간의 요일 가져오기
+ *
+ * @param {*} lang
+ * @param {*} key
+ * @returns
+ */
+function getYoil(lang = "en", key = 0) {
+    const yoil = {
+        Eng: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        Kor: ["일", "월", "화", "수", "목", "금", "토"]
+    };
+
+	switch (lang) {
+		case "ko":
+			return yoil.Kor[key];
+		default:
+			return yoil.Eng[key];
+	}
+}
